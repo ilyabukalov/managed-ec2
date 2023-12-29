302d87fa02d7b07d1d7750415b253b385ac2c425
ilyabukalov <ilyabukalov@gmail.com>
2023-12-29 18:48:30 +0700

    server and roles

--- prepare.sh
+++ prepare.sh
@@ -2,3 +2,11 @@ npm run cdk bootstrap aws://123456789012/us-east-1
 #
 ssh-keygen -m PEM -f ~/.ssh/aws_rsa_personal
 export KEY_FILE_NAME=.ssh/aws_rsa_personal
+# aws ssm get-parameters --names /aws/service/canonical/ubuntu/server/22.04/stable/current/amd64/hvm/ebs-gp2/ami-id
+# 20230919 instead of "current"
+export MACHINE_IMAGE_ID=/aws/service/canonical/ubuntu/server/22.04/stable/20230919/amd64/hvm/ebs-gp2/ami-id
+# extraxt root device name:
+# aws ec2 describe-images --region us-east-1 --image-ids ami-0fc5d935ebf8bc3bc
+export MACHINE_ROOT_VOLUME=/dev/sda1
+# folder in ~/
+export CODEBASE_FOLDER=firefly
