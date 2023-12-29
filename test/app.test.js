a06db51e4a1099f74c5fe16fd8d2c01e0ad6a70d
ilyabukalov <ilyabukalov@gmail.com>
2023-12-29 18:48:30 +0700

    bug fix environment

--- test/app.test.js
+++ test/app.test.js
@@ -1,14 +1,17 @@
-const cdk = require('aws-cdk-lib');
+const { App } = require('aws-cdk-lib');
 const { Match, Template } = require('aws-cdk-lib/assertions');
-const SampleApp = require('../lib/app-stack');
+const { AppStack } = require('../lib/app-stack');
 
 test('Created', () => {
-  const app = new cdk.App();
+  const app = new App();
   // WHEN
-  const stack = new SampleApp.AppStack(app, 'MyTestStack');
+  const stack = new AppStack(app, 'MyTestStack', { env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }});
   // THEN
   const template = Template.fromStack(stack);
   //template.hasResourceProperties('......', {});
 
   //template.resourceCountIs('AWS::SNS::Topic', 1);
+
+  // for the full of generated CloudFormation JSON match check
+  //expect(template.toJSON()).toMatchInlineSnapshot
 });
